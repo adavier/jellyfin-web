7d956043aa99c2498e71c27bed7daa915c3c0547
dkanada <dkanada@users.noreply.github.com>
2020-05-05 23:02:05 +0900

    basic skeleton for epub reader

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -501,6 +501,7 @@ var AppInfo = {};
             "components/htmlaudioplayer/plugin",
             "components/htmlvideoplayer/plugin",
             "components/photoplayer/plugin",
+            "components/bookplayer/plugin",
             "components/youtubeplayer/plugin",
             "components/backdropscreensaver/plugin",
             "components/logoscreensaver/plugin"
@@ -684,6 +685,7 @@ var AppInfo = {};
                     "fetch",
                     "flvjs",
                     "jstree",
+                    "epubjs",
                     "jQuery",
                     "hlsjs",
                     "howler",
